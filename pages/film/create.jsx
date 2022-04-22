872804fc902aeb5aca18820e77ba9fb61b4daab6
Valerio <valerio.valletta@icloud.com>
2022-04-22 16:02:22 +0200

    add create form not working

--- pages/film/create.jsx
+++ pages/film/create.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { Grid, TextField, Button } from "@mui/material";
+import { useState } from "react";
 
 const create = () => {
-    return <div>create</div>;
+    const [title, setTitle] = useState("");
+    return (
+        <Grid container justifyContent={"center"} alignItems="center">
+            <form>
+                <Grid
+                    container
+                    flexDirection={"column"}
+                    justifyContent={"center"}
+                    alignItems="center"
+                >
+                    <TextField
+                        id="outlined-basic"
+                        label="Title"
+                        variant="outlined"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <Button variant="contained" fullWidth>
+                        CONFIRM
+                    </Button>
+                </Grid>
+            </form>
+        </Grid>
+    );
 };
 
 export default create;
