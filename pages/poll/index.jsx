efa654377874cd35f50e68e2575342c8d26d5ed4
Valerio <valerio.valletta@icloud.com>
2022-05-03 15:12:58 +0200

    add home button in poll/index.js

--- pages/poll/index.jsx
+++ pages/poll/index.jsx
@@ -8,6 +8,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import { useRouter } from "next/router";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -28,28 +30,38 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
         border: 0,
     },
 }));
+
 const index = ({ polls }) => {
+    const router = useRouter();
+
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 700 }} aria-label="customized table">
-                <TableHead>
-                    <TableRow>
-                        <StyledTableCell>Film 1</StyledTableCell>
-                        <StyledTableCell>Film 2</StyledTableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {polls.map((row, i) => (
-                        <StyledTableRow key={i}>
-                            <StyledTableCell component="th" scope="row">
-                                {row.film1.title}
-                            </StyledTableCell>
-                            <StyledTableCell>{row.film2.title}</StyledTableCell>
-                        </StyledTableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Film 1</StyledTableCell>
+                            <StyledTableCell>Film 2</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {polls.map((row, i) => (
+                            <StyledTableRow key={i}>
+                                <StyledTableCell component="th" scope="row">
+                                    {row.film1.title}
+                                </StyledTableCell>
+                                <StyledTableCell>
+                                    {row.film2.title}
+                                </StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Button variant="contained" onClick={() => router.push("/")}>
+                HOME
+            </Button>
+        </>
     );
 };
 export const getServerSideProps = async ({ req }) => {
